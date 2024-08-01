147b7562ae2b14df243edce2816a0170a37aaeee
Pratham-Mishra04 <prathamm0409@gmail.com>
2024-08-01 16:13:02 +0530

    feat: minor fix

--- backend/helpers/logger.go
+++ backend/helpers/logger.go
@@ -13,11 +13,12 @@ func LogUnAuthorizedAccess(c *fiber.Ctx) {
 	method := c.Method()
 	url := c.OriginalURL()
 	apiToken := c.Get("api-token", "Not Provided")
+	origin := c.Get("Origin", "Not Provided")
 	clientIP := c.IP()
 
 	logDescription := fmt.Sprintf(
-		"Unauthorized Access Attempt\nMethod: %s\nURL: %s\nClient IP: %s\nAPI-Token: %s",
-		method, url, clientIP, apiToken,
+		"Unauthorized Access Attempt\nMethod: %s\nURL: %s\nOrigin: %s\nClient IP: %s\nAPI-Token: %s",
+		method, url, origin, clientIP, apiToken,
 	)
 
 	var log models.Log
